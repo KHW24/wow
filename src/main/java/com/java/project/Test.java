050b3fdb77c0af42f88bea82b4ec793434ef1009
KHW24 <rlagpdnjs1266@gmail.com>
2021-03-18 09:59:06 +0900

    text 파일 삭제

--- src/main/java/com/java/project/Test.java
+++
@@ -1,8 +0,0 @@
-package com.java.project;
-
-public class Test {
-public static void main(String[] args) {
-	System.out.println("정지수테스트입니다.!");
-	
- }
-}
--- src/main/java/com/java/project/Test01.java
+++
@@ -1,7 +0,0 @@
-package com.java.project;
-
-public class Test01 {
-	public static void main(String[] args) {
-	System.out.println("윤연오테스트입니다.!");
-	}
-}
