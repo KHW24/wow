999b7042e27be5f3f1d0cc9dab2ccebe9901411c
everglow0307 <wjdwltn1115@gmail.com>
2021-03-23 11:46:26 +0900

    test

--- src/main/java/com/kh/project/member/controller/TestController.java
+++ src/main/java/com/kh/project/member/controller/TestController.java
@@ -3,6 +3,11 @@ package com.kh.project.member.controller;
 public class TestController {
 	public static void main(String[] args) {
 		System.out.println("xptm");	
+		System.out.println("xptm");	
+		System.out.println("xptm");	
+		System.out.println("xptm");	
+		System.out.println("xptm");	
+		System.out.println("xptm");	
 	}
   
 }
