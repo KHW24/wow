9ef584bf51332fdc031a1f2a28bf6d423acd9e43
CookieYi <nomadanne1@naver.com>
2021-03-29 11:56:38 +0900

    이은지 - Board

--- src/main/java/com/kh/project/board/dao/BoardDao.java
+++ src/main/java/com/kh/project/board/dao/BoardDao.java
@@ -1,5 +1,8 @@
 package com.kh.project.board.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BoardDao {
 
 }
--- src/main/java/com/kh/project/board/service/BoardServiceImpl.java
+++ src/main/java/com/kh/project/board/service/BoardServiceImpl.java
@@ -1,5 +1,8 @@
 package com.kh.project.board.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardServiceImpl implements BoardService {
 
 	
