4feaedeffe68db5f6556d327c2f611936a087212
Mina <Mina@DESKTOP-OPCAT5K>
2021-03-29 20:23:47 +0900

    Mshipdata Controller, Service 틀

---
+++ src/main/java/com/kh/project/mshipdata/controller/MshipdataController.java
@@ -0,0 +1,5 @@
+package com.kh.project.mshipdata.controller;
+
+public class MshipdataController {
+
+}
---
+++ src/main/java/com/kh/project/mshipdata/service/MshipdataService.java
@@ -0,0 +1,5 @@
+package com.kh.project.mshipdata.service;
+
+public interface MshipdataService {
+
+}
